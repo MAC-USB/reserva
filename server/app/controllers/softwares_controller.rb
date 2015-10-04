2dbbff5bc2f6fb968f0a43576c2eae713c91918d
Amin Arria <arria.amin@gmail.com>
2015-10-04 17:34:46 -0430

    Implementado R (index y show)

--- server/app/controllers/softwares_controller.rb
+++ server/app/controllers/softwares_controller.rb
@@ -1,2 +1,18 @@
 class SoftwaresController < ApplicationController
+   
+   # Cuando se modifique la sala para ser una referencia
+   # probablemente se debera modificar.
+   # Lista del software de una sala
+   def index
+      @sw_list = Software.where(sala: params[:sala_id])
+
+      render json: @sw_list
+   end
+
+   # Detalles del software
+   def show
+      @sw = Software.find(params[:id])
+
+      render json: @sw
+   end
 end
