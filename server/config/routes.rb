a50f2a3f47529b5c15643e664e2376f31fcbd441
Amin Arria <arria.amin@gmail.com>
2015-10-13 17:43:23 -0430

    Ruta salas cambiado id por nombre

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :salas do |ruta_sala|
+  resources :salas, param: :nombre do |ruta_sala|
     delete "#{ruta_sala}/softwares" => 'softwares#destroyAll'
     resources :softwares, only: [:index, :show, :create, :destroy, :update]
   end
