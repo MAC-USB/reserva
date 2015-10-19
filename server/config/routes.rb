295a0d9f72d5a312472fc83a01546c7c65b26a02
Amin Arria <arria.amin@gmail.com>
2015-10-19 18:29:53 -0430

    Arreglado update y destroy

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :salas, param: :nombre do |ruta_sala|
   	delete "#{ruta_sala}/softwares" => 'softwares#destroyAll'
-  	resources :softwares, only: [:index, :show,  :update]
+  	resources :softwares, only: [:index, :show, :create, :destroy, :update]
   end
 
   resources :materias, param: :codigo, only: [:index, :create, :show, :update, :destroy]
