010c995ceb618c1788f00d59b962f04b443224b0
Amin Arria <arria.amin@gmail.com>
2015-10-20 14:54:34 -0430

    Validaciones sala_nombre y materia

--- server/app/models/reserva.rb
+++ server/app/models/reserva.rb
@@ -1,4 +1,6 @@
 class Reserva < ActiveRecord::Base
   belongs_to :materia
   belongs_to :sala, primary_key: "nombre", foreign_key: "sala_nombre"
+  validates :sala_nombre, presence: true
+  validates :materia, presence: true
 end
