b4cceebce7e172655e1e79ed6e57ac0a5ac813ad
herramientasweb <frammnm@gmail.com>
2015-10-27 20:33:39 +0000

    Agregado UD y destroyAll al controlador de reserva,se modifico el archivo de rutas.

--- server/app/controllers/reserva_controller.rb
+++ server/app/controllers/reserva_controller.rb
@@ -7,8 +7,26 @@ class ReservaController < ApplicationController
   end
 
   def update
+    @reserva = Reserva.find_by(id: params[:id])
+    if not @reserva
+      render nothing: true, status: :not_found
+    elsif @reserva.update_attributes(params.require(:reserva).permit(:sala_nombre, :materia))
+      render nothing: true, status: :ok
+    else
+      render nothing: true, status: :bad_request
+    end
   end
 
   def destroy
+     @reserva = Reserva.find_by(id: params[:id])
+    if @reserva
+      @reserva.destroy
+    end
+    render nothing: true, status: :ok
+  end
+  
+  def destroyAll
+    Reserva.destroy_all(sala_nombre: params[:sala_nombre])
+    render nothing: true, status: :ok
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   resources :salas, param: :nombre do |ruta_sala|
   	delete "#{ruta_sala}/softwares" => 'softwares#destroyAll'
   	resources :softwares, only: [:index, :show, :create, :destroy, :update]
-  end
-
-  resources :salas, param: :nombre do |ruta_sala|
-  	delete "#{ruta_sala}/reservas" => 'reservas#destroyAll'
+    delete "#{ruta_sala}/reservas" => 'reservas#destroyAll'
   	resources :reservas, only: [:index, :show, :create, :update, :destroy]
   end
 
