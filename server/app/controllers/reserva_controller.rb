e5d20adee22c3ab7d27ec98c1f80c31a9e79f68e
Sofia Bravo <sofiabravo103@gmail.com>
2015-10-28 21:03:48 -0430

    Agregardos CR e Index al controlador de reserva

--- server/app/controllers/reserva_controller.rb
+++ server/app/controllers/reserva_controller.rb
@@ -1,9 +1,27 @@
 class ReservaController < ApplicationController
 
+  def create
+    @reserva = Reserva.new(params.require(:reserva)
+      .permit(:sala_nombre, :materia, :video_beam, :requerimientos))
+    if @reserva.save!
+      render nothing: true, status: :created
+    else
+      render nothing: true, status: :conflict
+    end
+  end
+
   def show
+    @reserva = Reserva.find(params[:id])
+    if @reserva
+      render json: @reserva
+    else
+      render nothing: true, status: :not_found
+    end
   end
 
   def index
+    @list = Reserva.all
+    render json: @list
   end
 
   def update
